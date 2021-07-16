570e66b4b06409620afb2a2d1a198e10cddcead0
kkopec <s15509@pjwstk.edu.pl>
2021-07-16 13:37:29 +0200

    Timeline Section + Fooer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -26,7 +26,7 @@ const Header = () => (
         </Link>
       </li>
       <li>
-        <Link href="#abou">
+        <Link href="#about">
           <NavLink>About</NavLink>
         </Link>
       </li>
