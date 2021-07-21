560a03d407f1998b257f64eaea0b2dc4b05fd230
kkopec <s15509@pjwstk.edu.pl>
2021-07-21 15:46:11 +0200

    Footer and Styling .2

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin, AiFillFacebook } from 'react-icons/ai';
+
+import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './styles';
+import { SocialIcons } from '../../styles/components/globalComponents';
+
+const Footer = () => {
+    return (
+        <FooterWrapper>
+            <LinkList>
+                <LinkColumn>
+                    <LinkTitle>Call</LinkTitle>
+                    <LinkItem href="tel:508-400-121">508-400-121</LinkItem>
+                </LinkColumn>
+                <LinkColumn>
+                    <LinkTitle>Email</LinkTitle>
+                    <LinkItem href="mailto:krzysztof.praca19@gmail.com">krzysztof.praca19@gmail.com</LinkItem>
+                </LinkColumn>
+            </LinkList>
+            <SocialIconsContainer>
+                <CompanyContainer>
+                    <Slogan>“Code is like humor. When you have to explain it, it’s bad.” – Cory House</Slogan>
+                </CompanyContainer>
+                <SocialContainer>
+                    <SocialIcons href="https://github.com">
+                        <AiFillGithub size="3rem" />
+                    </SocialIcons>
+                    <SocialIcons href="https://linkedin.com">
+                        <AiFillLinkedin size="3rem" />
+                    </SocialIcons>
+                    <SocialIcons href="https://instragram.com">
+                        <AiFillInstagram size="3rem" />
+                    </SocialIcons>
+                    <SocialIcons href="https://www.facebook.com/krzysiek.kopec.58">
+                        <AiFillFacebook size="3rem" />
+                    </SocialIcons>
+                </SocialContainer>
+            </SocialIconsContainer>
+        </FooterWrapper>
+    );
+};
+
+export default Footer;
