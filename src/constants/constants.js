bedd7a9bf64167ef5c737abca8935a9dd83ff4ed
kkopec <s15509@pjwstk.edu.pl>
2021-08-12 16:22:33 +0200

    3rd Procject - Event4You

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,11 +1,12 @@
 export const projects = [
+
   {
-    title: 'Weather App',
-    description: "My own implementation of a classic weather app.  In the future this will be expanded by including a back-end database to enable the user to directly select cities and countries, rather than having to enter those variables manually.",
-    image: '/images/Weather.png',
-    tags: ['React', 'Styled-Components', "Axios"],
-    source: 'https://weather-app-kkopec.netlify.app',
-    visit: 'https://github.com/kkopec11/Weather.App',
+    title: 'Event4You',
+    description: "MERN type application with Redux for authorization - also user is able to login by GoogleOAuth. Events photos are storen on MongoDB with base64 encrypting. Each event can be liked by the other users. In the futhure: there will be added a 'comment' funcionality, searching option fopr tags, adding event to favourite and GoogleMaps for locating the event's place/adress. ",
+    image: '/images/Event4You.png',
+    tags: ['React', 'MaterialUI', 'Redux', 'MERN'],
+    source: 'https://event4you-kkopec.netlify.app/',
+    visit: 'https://github.com/kkopec11/Event4You',
     id: 0,
   },
   {
@@ -18,12 +19,12 @@ export const projects = [
     id: 1,
   },
   {
-    title: 'Photo Album',
-    description: "A photo album web-app.  One of my first projects when I was learning MaterialUI library.  I am going to be adding further functionalities such as account creation.",
-    image: '/images/PhotoAlbum.png',
-    tags: ['React', 'MaterialUI'],
-    source: 'https://photo-album-kkopec.netlify.app',
-    visit: 'https://github.com/kkopec11/PhotoAlbum',
+    title: 'Weather App',
+    description: "My own implementation of a classic weather app.  In the future this will be expanded by including a back-end database to enable the user to directly select cities and countries, rather than having to enter those variables manually.",
+    image: '/images/Weather.png',
+    tags: ['React', 'Styled-Components', "Axios"],
+    source: 'https://weather-app-kkopec.netlify.app',
+    visit: 'https://github.com/kkopec11/Weather.App',
     id: 2,
   },
 ];
