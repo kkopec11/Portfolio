defad8b3a5768a20fab6dab4102b2750ef70a1c5
KrzysztofKopec <kkopec119@gmail.com>
2023-11-11 17:47:56 +0100

    About me section and CV update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,37 +1,70 @@
 export const projects = [
-
   {
-    title: 'Event4You',
-    description: "MERN type application with Redux for authorization. Pictures are stored on MongoDB with base64 encrypting.  I am going to be adding further functionalities, such as commentary section, searching for tags, adding event to favourite and GoogleMaps. ",
-    image: '/images/Event4You.png',
-    tags: ['React', 'MaterialUI', 'Redux', 'MERN'],
-    source: 'https://event4you-kkopec.netlify.app/',
-    visit: 'https://github.com/kkopec11/Event4You',
+    title: "Event4You",
+    description:
+      "MERN type application with Redux for authorization. Pictures are stored on MongoDB with base64 encrypting.  I am going to be adding further functionalities, such as commentary section, searching for tags, adding event to favourite and GoogleMaps. ",
+    image: "/images/Event4You.png",
+    tags: ["React", "MaterialUI", "Redux", "MERN"],
+    source: "https://event4you-kkopec.netlify.app/",
+    visit: "https://github.com/kkopec11/Event4You",
     id: 0,
   },
   {
-    title: 'Photo Album',
-    description: "A photo album web-app. One of my first projects when I was learning MaterialUI library. I am going to be adding further functionalities such as account creation, saving photos to favourite and tags with searching field.",
-    image: '/images/PhotoAlbum.png',
-    tags: ['React', 'MaterialUI'],
-    source: 'https://photo-album-kkopec.netlify.app',
-    visit: 'https://github.com/kkopec11/PhotoAlbum',
+    title: "Photo Album",
+    description:
+      "A photo album web-app. One of my first projects when I was learning MaterialUI library. I am going to be adding further functionalities such as account creation, saving photos to favourite and tags with searching field.",
+    image: "/images/PhotoAlbum.png",
+    tags: ["React", "MaterialUI"],
+    source: "https://photo-album-kkopec.netlify.app",
+    visit: "https://github.com/kkopec11/PhotoAlbum",
     id: 1,
   },
   {
-    title: 'Weather App',
-    description: "My own implementation of a classic weather app.  In the future this will be expanded by including a back-end database to enable the user to directly select cities and countries, rather than having to enter those variables manually.",
-    image: '/images/Weather.png',
-    tags: ['React', 'Styled-Components', "Axios"],
-    source: 'https://weather-app-kkopec.netlify.app',
-    visit: 'https://github.com/kkopec11/Weather.App',
+    title: "Weather App",
+    description:
+      "My own implementation of a classic weather app.  In the future this will be expanded by including a back-end database to enable the user to directly select cities and countries, rather than having to enter those variables manually.",
+    image: "/images/Weather.png",
+    tags: ["React", "Styled-Components", "Axios"],
+    source: "https://weather-app-kkopec.netlify.app",
+    visit: "https://github.com/kkopec11/Weather.App",
     id: 2,
   },
 ];
 
 export const TimeLineData = [
-  { image: '/images/pjatk-logo.jpg', year: 2016, title: 'Polish-Japanese Academy of Information Technology', text: 'Whilst studying as an IT engineer with data-base specialization, my engineering work centred on creating a UI interface using a React library. The most valuable experience for me was using Redux to manipulate the state of the interface appearance according to the different statuses of users and their roles.', id: 0 },
-  { image: '/images/ichtj.png', year: 2018, title: 'Institute of Nuclear Chemistry and Technology', text: 'Direct and remote support of users in their daily work at their workstations.  Hardware and software management.  Installation and configuration of workstations and other IT equipment. This required flexibility, problem solving and customer communication skills.', id: 1 },
-  { image: '/images/accenture1.png', year: 2020, title: 'Accenture', text: 'A year-long internship in an international project supporting a team of analysts using SAS software. Programming components for user interface in React language. Working with Oracle database.', id: 2 },
-  { image: '/images/accenture2.jpg', year: 2021, title: 'Accenture Applied Inteligent', text: 'I have been improving a telecommunication system by adding new functions and infrastructure, such as the triggers responsible for sending SMS messages.  I have presented the progress on a live demo for the client after each sprint.  I operated on Oracle databases working closely with the QA team as well as performing tests individually.', id: 3 },
+  {
+    image: "/images/accenture-song.png",
+    year: 2022,
+    title: "Accenture Song",
+    text: "Implementation of user interfaces based on functional mockups and graphic designs. Collaboration with the UX team. Creating new and developing existing applications. Participating in implementation projects for clients.",
+    id: 0,
+  },
+  {
+    image: "/images/accenture2.jpg",
+    year: 2021,
+    title: "Accenture Applied Inteligent",
+    text: "I have been improving a telecommunication system by adding new functions and infrastructure, such as the triggers responsible for sending SMS messages.  I have presented the progress on a live demo for the client after each sprint.  I operated on Oracle databases working closely with the QA team as well as performing tests individually.",
+    id: 1,
+  },
+  {
+    image: "/images/accenture1.png",
+    year: 2020,
+    title: "Accenture",
+    text: "A year-long internship in an international project supporting a team of analysts using SAS software. Programming components for user interface in React language. Working with Oracle database.",
+    id: 2,
+  },
+  {
+    image: "/images/ichtj.png",
+    year: 2018,
+    title: "Institute of Nuclear Chemistry and Technology",
+    text: "Direct and remote support of users in their daily work at their workstations.  Hardware and software management.  Installation and configuration of workstations and other IT equipment. This required flexibility, problem solving and customer communication skills.",
+    id: 3,
+  },
+  {
+    image: "/images/pjatk-logo.jpg",
+    year: 2016,
+    title: "Polish-Japanese Academy of Information Technology",
+    text: "Whilst studying as an IT engineer with data-base specialization, my engineering work centred on creating a UI interface using a React library. The most valuable experience for me was using Redux to manipulate the state of the interface appearance according to the different statuses of users and their roles.",
+    id: 4,
+  },
 ];
